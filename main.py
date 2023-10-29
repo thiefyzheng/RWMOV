8b4b90eda08b98bce403090260cce649ea93bf5b
thiefyzheng <65657326+thiefyzheng@users.noreply.github.com>
2023-10-29 22:07:03 +0800

    srt

--- main.py
+++ main.py
@@ -38,15 +38,16 @@ def convert_to_mp4(input_file_path):
     # Use the specific ffmpeg settings provided
     if platform.system() == 'Darwin':  # Check if the operating system is macOS
         subprocess.run(
-            ['ffmpeg', '-i', input_file_path, '-vcodec', 'hevc_videotoolbox', '-b:v', '6000k', '-tag:v', 'hvc1', '-c:a', 'eac3', '-b:a', '224k', output_file_path],
+            ['ffmpeg', '-y', '-i', input_file_path, '-vcodec', 'hevc_videotoolbox', '-b:v', '6000k', '-tag:v', 'hvc1', '-c:a', 'eac3', '-b:a', '224k', '-scodec', 'mov_text', output_file_path],
             check=True)
     else:  # Default to CUDA for non-macOS systems
         subprocess.run(
-            ['ffmpeg', '-i', input_file_path, '-vcodec', 'hevc_nvenc', '-b:v', '6000k', '-tag:v', 'hvc1', '-c:a', 'eac3', '-b:a', '224k',
+            ['ffmpeg', '-y', '-i', input_file_path, '-vcodec', 'hevc_nvenc', '-b:v', '6000k', '-tag:v', 'hvc1', '-c:a', 'eac3', '-b:a', '224k', '-scodec', 'mov_text',
              output_file_path], check=True)
 
     text.set(text.get() + '\nConverted: ' + os.path.basename(
         input_file_path))  # Update the label text with just the filename
+
 
 def open_file_dialog():
     path = filedialog.askdirectory()  # Open the file dialog to select a directory
