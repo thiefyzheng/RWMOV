c28158e389901d0c61c1b8e02883986de4818ac1
thiefyzheng <65657326+thiefyzheng@users.noreply.github.com>
2023-10-29 12:31:44 +0800

    add windows and nix support

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import tkinter as tk
 from tkinterdnd2 import DND_FILES, TkinterDnD
 
 def drop(event):
-    input_file_path = event.data[1:-1]  # Remove the {} characters
+    input_file_path = event.data
+    # Check if the operating system is Windows
+    if os.name == 'nt':
+        # Remove the {} characters for Windows
+        input_file_path = input_file_path[1:-1]
     output_file_path = os.path.splitext(input_file_path)[0] + '.mov'
     subprocess.run(['ffmpeg', '-i', input_file_path, '-c', 'copy', output_file_path], check=True)
 
